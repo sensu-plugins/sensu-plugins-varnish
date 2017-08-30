1ce6f5b2eb1c728e45a3985e7c2f63692f187ee4
Shaun Rutherford <shaun.rutherford@tech-corps.com>
2017-08-29 18:27:54 -0700

    Backend health command changed on new Varnish versions (#15)
    * adding option to status check for new varnish backend health command
    
    * updating changelog to 1.1.2 describing new backend health status check
    
    * changing or to || per robucop guidelines

--- bin/check-varnish-status.rb
+++ bin/check-varnish-status.rb
@@ -88,7 +88,7 @@ class CheckVarnishStatus < Sensu::Plugin::Check::CLI
       end
     end
 
-    if config[:command] == 'debug.health'
+    if config[:command] == 'debug.health' || config[:command] == 'backend.list'
       if command.downcase.include? 'sick'
         critical 'Sick backends detected'
       else
